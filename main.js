9d030f3a641c59b5b037cbab3d5a57e67b938d6b
JayrajSinvhal <76859265+JayrajSinvhal@users.noreply.github.com>
2021-10-14 19:44:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+var screen = document.getElementById("screen");
+buttons = document.querySelectorAll("button");
+var screenValue = "";
+for (item of buttons) {
+  item.addEventListener("click", (e) => {
+    buttonText = e.target.innerText;
+    console.log("Button is", buttonText);
+    if (buttonText === "X") {
+      buttonText = "*";
+      screenValue += buttonText;
+      screen.value = screenValue;
+    } else if (buttonText === "=") {
+      screen.value = eval(screenValue);
+    } else if (buttonText === "C") {
+      screenValue = "";
+      screen.value = screenValue;
+    } else {
+      screenValue += buttonText;
+      screen.value = screenValue;
+    }
+  });
+}
